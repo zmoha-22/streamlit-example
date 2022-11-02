c8eac72d3cb8afc2419ae0d90b742840fdfcdeeb
zmoha-22 <110062384+zmoha-22@users.noreply.github.com>
2022-11-01 19:18:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from TIMESERIES_IGNITION_VIZ;")
+rows = run_query("SELECT * from TIMESERIES_IGNITION_VIZ1;")
 
 # Print results.
 for row in rows:
